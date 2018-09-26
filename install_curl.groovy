e644f2bd7a22d0a66d8e082ea6f5d6411d34b914
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-26 14:25:32 +0530

    Create install_curl.groovy

---
+++ install_curl.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+def call(body) {
+	
+
+	        def config = [:]
+	        body.resolveStrategy = Closure.DELEGATE_FIRST
+	        body.delegate = config
+	        body()
+	
+
+
+	script {
+			  sh '''
+			  	sudo apt-get -y install curl
+			   '''
+		   }
+}
+
+return this;
+
